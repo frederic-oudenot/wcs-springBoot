65fc13affb7b845ecb14df9fdf570812fcdba52b
Frédéric Oudenot <f.oudenot@gmail.com>
2025-01-13 19:24:52 +0100

    build: model-Article

--- src/main/java/org/wildcodeschool/myblog/MyBlogApplication.java
+++ src/main/java/org/wildcodeschool/myblog/MyBlogApplication.java
@@ -2,20 +2,28 @@ package org.wildcodeschool.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
 
 @SpringBootApplication
-@RestController
 public class MyBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBlogApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String sayHello(){
-		return "Hello World";
+	@Configuration
+	public class AppConfig {
+
+		@Bean
+		public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+			PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+			configurer.setLocation(new FileSystemResource(".env"));
+			return configurer;
+		}
+
 	}
 
 }
