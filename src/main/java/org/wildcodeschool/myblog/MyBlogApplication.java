4539243438760d534bf5dd047728ecf3391a576c
Frédéric Oudenot <f.oudenot@gmail.com>
2025-01-13 11:17:00 +0100

    build: MyBlog

---
+++ src/main/java/org/wildcodeschool/myblog/MyBlogApplication.java
@@ -0,0 +1,21 @@
+package org.wildcodeschool.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(){
+		return "Hello World";
+	}
+
+}
